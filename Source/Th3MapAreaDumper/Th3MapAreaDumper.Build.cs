7ae1ef97d9b6803bb7dddec0e1e73e3b993e9f11
Angel Pons <th3fanbus@gmail.com>
2024-11-08 14:07:51 +0100

    Update to FG 1.0
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- Source/Th3MapAreaDumper/Th3MapAreaDumper.Build.cs
+++ Source/Th3MapAreaDumper/Th3MapAreaDumper.Build.cs
@@ -1,47 +1,25 @@
 /* SPDX-License-Identifier: MPL-2.0 */
 
 using UnrealBuildTool;
-using System.IO;
-using System;
 
 public class Th3MapAreaDumper : ModuleRules
 {
 	public Th3MapAreaDumper(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		bUseUnity = false;
+    {
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        ShadowVariableWarningLevel = WarningLevel.Error;
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bLegacyParentIncludePaths = false;
+        CppStandard = CppStandardVersion.Cpp20;
+        bUseUnity = false;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core", "CoreUObject",
-			"Engine",
-			"DeveloperSettings",
-			"PhysicsCore",
-			"InputCore",
-			//"OnlineSubsystem", "OnlineSubsystemUtils", "OnlineSubsystemNull",
-			//"SignificanceManager",
-			"GeometryCollectionEngine",
-			//"ChaosVehiclesCore", "ChaosVehicles", "ChaosSolverEngine",
-			"AnimGraphRuntime",
-			//"AkAudio",
-			"AssetRegistry",
-			"NavigationSystem",
-			//"ReplicationGraph",
-			"AIModule",
-			"GameplayTasks",
-			"SlateCore", "Slate", "UMG",
-			//"InstancedSplines",
-			"RenderCore",
-			"CinematicCamera",
-			"Foliage",
-			//"Niagara",
-			//"EnhancedInput",
-			//"GameplayCameras",
-			//"TemplateSequence",
-			"NetCore",
-			"GameplayTags",
-			"Json", "JsonUtilities",
-			"DummyHeaders",
-			"FactoryGame", "SML"
-		});
-	}
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core", "CoreUObject", "Engine",
+            "DeveloperSettings", "PhysicsCore", "InputCore",
+            "AssetRegistry", "RenderCore", "RHI",
+            "SlateCore", "Slate", "UMG", "GameplayTags",
+            "Json", "JsonUtilities",
+            "DummyHeaders", "FactoryGame", "SML",
+        });
+    }
 }
